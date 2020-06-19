fbbb220b3f12bb49a6d34931bb9da75993c32a98
Rincevent42 <porvooc@gmail.com>
2020-06-19 12:14:41 +0200

    add points 2 and 3

--- index.js
+++ index.js
@@ -11,12 +11,82 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// en tant qu'utilisateur, je veux pouvoir créer une nouvelle playlist.thode
-app.post('playlist', (req, res) => {
+// en tant qu'utilisateur, je veux pouvoir créer une nouvelle playlist
+app.post('/playlist', (req, res) => {
   const data = req.body;
-  console.log(data);
-
+// connexion à la base de données, et insertion d'une playlist
+  connection.query('INSERT INTO playlist SET ?', data, (err, results) => {
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Erreur lors de la sauvegarde d'une playlist");
+    } else {
+      // We use the insertId attribute of results to build the WHERE clause
+      return connection.query('SELECT * FROM playlist WHERE id = ?', results.insertId, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });o
+        }
+        // If all went well, records is an array, from which we use the 1st item
+        const playlist = records[0];
+        return res
+          .status(200)
+          .json(playlist);
+      });
+    }
+  });
 });
+
+// en tant qu'utilisateur, je veux pouvoir consulter une playlist en renseignant son id
+// dans l'url (juste ses données propres, pas les pistes associées).
+app.get ('/playlist/:id', (req, res) => {
+  const idPlaylist = req.params.id;
+  connection.query('SELECT * FROM playlist WHERE id = ?', idPlaylist, (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+        sql: err.sql
+      });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+// en tant qu'utilisateur, je veux créer et affecter un morceau à une playlist.
+// BESOINS :
+// - id de la playlist dans l'URL
+// - champs de la piste dans le body
+app.post('/track/:playlist', (req, res) => {
+  const idPlaylist = req.params.playlist; // récupère l'id de la playlist
+  const data = { ...req.body, playlist_id: idPlaylist }; // combine les données du body + l'id playlist
+  console.log(data);
+  // connexion à la base de données, et insertion d'une piste
+  connection.query('INSERT INTO track SET ?', data, (err, results) => {
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Erreur lors de la création d'une piste");
+    } else {
+      // We use the insertId attribute of results to build the WHERE clause
+      return connection.query('SELECT * FROM track WHERE id = ?', results.insertId, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });o
+        }
+        // If all went well, records is an array, from which we use the 1st item
+        const playlist = records[0];
+        return res
+          .status(200)
+          .json(playlist);
+      });
+    }
+  });
+})
 
 // Lancement du serveur
 app.listen(process.env.PORT, (err) => {
