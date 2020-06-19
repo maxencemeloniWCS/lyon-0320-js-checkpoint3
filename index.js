30abde4330923ba88161ad0445835dc9e2529dea
Rincevent42 <porvooc@gmail.com>
2020-06-19 11:27:09 +0200

    Route add playlist almost done

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// en tant qu'utilisateur, je veux pouvoir créer une nouvelle playlist.thode
-app.post('playlist', (req, res) => {
+// en tant qu'utilisateur, je veux pouvoir créer une nouvelle playlist
+app.post('/playlist', (req, res) => {
   const data = req.body;
   console.log(data);
-
+// connexion à la base de données, et insertion d'une playlist
+  connection.query('INSERT INTO playlist SET ?', data, (err, results) => {
+    if (err) {
+      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      console.log(err);
+      res.status(500).send("Erreur lors de la sauvegarde d'une playlist");
+    } else {
+      // Si tout s'est bien passé, on envoie un statut "ok".
+      res.sendStatus(201);
+    }
+  });
 });
 
 // Lancement du serveur
