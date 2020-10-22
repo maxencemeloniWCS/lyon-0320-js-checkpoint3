1e04e4a51d4a561603be2db9984e7d07b14644c7
Marine Montailler <marinemontailler@gmail.com>
2020-10-22 12:24:19 +0200

    add .env file

--- datasource/mysql.js
+++ datasource/mysql.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv').config();
 module.exports = mysql.createConnection({
-    user: 'root',
-    password: '7290',
-    host: 'localhost',
-    database: 'playlist_db'
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE
 });
