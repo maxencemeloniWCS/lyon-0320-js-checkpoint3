a0e1c8ecd5f1b873177c4389ffd66a8bd47e715b
Madalina <madalina.zamfira@gmail.com>
2020-10-22 10:15:39 +0200

    replaced conf.js by .env

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const connection = require('./conf');
-
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
