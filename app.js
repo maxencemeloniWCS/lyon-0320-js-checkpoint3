32869b635a5ac5f6d9bb755ac60c9c8f525cd6d9
Madalina <madalina.zamfira@gmail.com>
2020-10-22 11:16:28 +0200

    create route to add a new playlist

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./datasource/mysql');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -14,3 +20,17 @@ app.get("/", function(req, res){
     res.send('All good')
 })
 
+//en tant qu'utilisateur, je veux pouvoir créer une nouvelle playlist.
+app.post('/playlist', (req, res) => {
+    const formData = req.body;
+    console.log(formData);
+    db.query(`INSERT INTO playlist SET ?`, [formData], (err, results) => {
+  
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la sauvegarde d'une playlist");
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  });
