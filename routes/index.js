6909482736ec0a7ecda63ac13272aacd0a283e20
Trad <trad.aidoud@gmail.com>
2020-10-22 10:55:22 +0200

    ok

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const db = require("../mysql/mysql");
 
 router.get("/", function (req, res) {
-    res.send("Arbre");
-  });
+  res.send("Arbre");
+});
 
 router.get("/playlist/:id", function (req, res) {
   const id = parseInt(req.params.id);
   db.query(
-    "SELECT title, genre FROM playlist WHERE id = ?",[id],
+    "SELECT title, genre FROM playlist WHERE id = ?",
+    [id],
     (err, results) => {
       if (err) {
         res.status(500).send({ message: "Erreur" });
@@ -21,18 +22,19 @@ router.get("/playlist/:id", function (req, res) {
 });
 
 router.get("/playlistTracks/:id", function (req, res) {
-    const id = parseInt(req.params.id);
-    db.query(
-      "SELECT track.title, track.artist, track.album_picture, track.youtube_url FROM track JOIN playlist ON track.playlist_id=?",[id],
-      (err, results) => {
-        if (err) {
-          res.status(500).send({ message: "Erreur", err });
-        } else {
-          res.status(200).send(results);
-        }
+  const id = parseInt(req.params.id);
+  db.query(
+    "SELECT track.title, track.artist, track.album_picture, track.youtube_url FROM track JOIN playlist ON track.playlist_id=?",
+    [id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send({ message: "Erreur", err });
+      } else {
+        res.status(200).send(results);
       }
-    );
-  });
+    }
+  );
+});
 
 router.post("/createPlaylist", function (req, res) {
   const title = req.body.title;
@@ -45,39 +47,88 @@ router.post("/createPlaylist", function (req, res) {
         console.log(err);
         res.status(500).send({ message: "Erreur" });
       } else {
-        res.status(200).send({ message: "post confirmed" });
+        res.status(200).send({ message: "post confirmed", results });
       }
     }
   );
 });
 
 router.post("/addTrack", function (req, res) {
-    db.query(
-      "INSERT INTO track (playlist_id, title, artist, album_picture, youtube_url) VALUES (?)",
-      [[req.body.playlist_id, req.body.title,req.body.artist, req.body.album_picture, req.body.youtube_url, ]],
-      (err, results) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send({ message: "Erreur" });
-        } else {
-          res.status(200).send({ message: "track add" });
-        }
+  db.query(
+    "INSERT INTO track (playlist_id, title, artist, album_picture, youtube_url) VALUES (?)",
+    [
+      [
+        req.body.playlist_id,
+        req.body.title,
+        req.body.artist,
+        req.body.album_picture,
+        req.body.youtube_url,
+      ],
+    ],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ message: "Erreur" });
+      } else {
+        res.status(200).send({ message: "track add", results });
       }
-    );
-  });
+    }
+  );
+});
 
-  router.delete("/delete/:id", (req, res) => {
-    db.query(
-      "DELETE FROM playlist WHERE id = ?", parseInt(req.params.id),
-      (err) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send("Erreur");
-        } else {
-          res.sendStatus(200);
-        }
+router.delete("/delete/:id", (req, res) => {
+  db.query(
+    "DELETE FROM playlist WHERE id = ?",
+    parseInt(req.params.id),
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur");
+      } else {
+        res.sendStatus(200);
       }
-    );
+    }
+  );
+});
+
+router.delete("/delTrackPlaylist/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  db.query("DELETE FROM track WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erreur", err);
+    } else {
+      res.status(200).send("morceau supprimé!");
+    }
   });
+});
+
+router.put("/renamePlaylist/:id", (req, res) => {
+  db.query(
+    "UPDATE playlist SET ? WHERE id = ?",
+    [req.body, parseInt(req.params.id)],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur", err);
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
+});
+
+router.put("/updateTrack/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const form = req.body;
+  db.query("UPDATE track SET ? WHERE id = ?", [form, id], (err) => {
+    if (err, results) {
+      console.log(err);
+      res.status(500).send("Erreur", err);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
 
 module.exports = router;
