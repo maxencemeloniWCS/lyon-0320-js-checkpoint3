8923303fe114e0ac5c52293287ef1f4d36986658
Marine Montailler <marinemontailler@gmail.com>
2020-10-22 11:40:59 +0200

    bonus 1 route /playlists and /tracks

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('./datasource/mysql');
+
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -14,6 +16,27 @@ const trackRouter = require('./routes/track');
 app.use('/playlist', playlistRouter);
 app.use('/track', trackRouter);
 
+//BONUS 1
+
+app.get('/playlists', (req,res)=>{
+    db.query('SELECT title, genre FROM playlist', (err,result)=>{
+        if(err){
+            res.status(500).send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+app.get('/tracks', (req,res)=>{
+    db.query('SELECT title, artiste, playlist_id FROM track', (err,result)=>{
+        if(err){
+            res.status(500).send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
