879a092ec8025e6abfa22ba432bb63d378412985
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-10-22 14:46:32 +0200

    remove node_module

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../db");
-const { route } = require("./track");
 
 router.post("/", (req, res) => {
   connection.query("INSERT INTO playlist SET ?", [req.body], (err, results) => {
